c198e1a4c3c7393bbed95bc08d0634cd4f2c6dee
Kim Capilla <kcapilla@Kims-MBP.attlocal.net>
2022-01-13 18:34:46 -0500

    adding more characters

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from re import fullmatch
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -21,16 +20,34 @@ db.connect()
 db.drop_tables([Character])
 db.create_tables([Character])
 
-jon = Character(full_name='Jon Snow', house_name='House Stark of Winterfell', quote='There is only one war that matters. The Great War. And it is here.')
-
+jon = Character(full_name='Jon Snow', house_name='House Stark of Winterfell', quote="If I fall, don't bring me back.")
+jon1 = Character(full_name='Jon Snow', house_name='House Stark of Winterfell', quote='There is only one war that matters. The Great War. And it is here.')
+jon2 = Character(full_name='Jon Snow', house_name='House Stark of Winterfell', quote='Love is the death of duty.')
+jon3 = Character(full_name='Jon Snow', house_name='House Stark of Winterfell', quote='Everything before the word \"but\" is horseshit.')
+jon4 = Character(full_name='Jon Snow', house_name='House Stark of Winterfell', quote="The war is not over. And I promise you, friend, the true enemy won't wait out the storm. He brings the storm.")
+sansa = Character(full_name='Sansa Stark', house_name='House Stark of Winterfell', quote="I hate the king more than any of them.")
+sansa1 = Character(full_name='Sansa Stark', house_name='House Stark of Winterfell', quote="No need to seize the last word, Lord Baelish. I'll assume it was something clever.")
+jaime = Character(full_name='Jaime Lannister', house_name='House Lannister of Casterly Rock', quote="By what right does the wolf judge the lion?")
+tyrion = Character(full_name='Tyrion Lannister', house_name='House Lannister of Casterly Rock', quote="Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you.")
+dany = Character(full_name='Daenerys Targaryen', house_name="House Targaryen of King's Landing", quote="I am the dragon's daughter, and I swear to you that those who would harm you will die screaming.")
 jon.save()
+jon1.save()
+jon2.save()
+jon3.save()
+jon4.save()
+sansa.save()
+sansa1.save()
+jaime.save()
+tyrion.save()
+dany.save()
+
 
 
 app = Flask(__name__)
 
 @app.route('/character', methods=['GET'])
-@app.route('character/<id>', methods=['GET'])
-def person(id=None):
+@app.route('/character/<id>', methods=['GET'])
+def character(id=None):
   if id:
     character = Character.get(Character.id == id)
     character = model_to_dict(character)
@@ -42,4 +59,4 @@ def person(id=None):
     return jsonify(char)
   
   
-app.run(port=900, debug=True)
+app.run(port=9000, debug=True)
